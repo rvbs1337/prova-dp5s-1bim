779a102bfa60b66467409372f63294ccf2213d68
Rubens <rubenspereira9@hotmail.com>
2024-05-04 15:56:51 -0300

    teste das rotas auxiliares comics

--- test/comic.test.ts
+++ test/comic.test.ts
@@ -3,10 +3,10 @@ import comicModel from '../src/comic/comic.schema'
 import app from '../app';
 
 
-describe('testando endpoints comics', ()=>{
-    it('Deve inserir um comic no banco',async () => {
+describe('testando endpoints comics', () => {
+    it('Deve inserir um comic no banco', async () => {
         const publicationDate = new Date();
-        
+
         const comicMock = {
             title: "Tarefa teste 1",
             description: "teste de insercao 1",
@@ -75,4 +75,30 @@ describe('testando endpoints comics', ()=>{
         expect(deleteResponse.status).toBe(200);
         expect(foundDeletedComic).toBeNull();
     });
+
+    it('Deve encontrar uma comic por um pedaço do titulo', async () => {
+        const titulo = "HEROES FOR HIRE VOL. 3: WORLD WAR HULK TPB"
+        const response = await request(app).get(`/comic/titulo/${titulo}`)
+
+        expect(response.status).toEqual(200)
+        expect(response.body[0].title).toContain(titulo)
+    })
+
+    it('Deve encontrar as comics por um pedaço do nome de um Criador', async () => {
+        const nome = "Tom"
+        const response = await request(app).get(`/comic/creator/${nome}`)
+
+        expect(response.status).toEqual(200)
+        expect(response.body[0].title).toBe("Avengers: The Initiative (2007) #4")
+    })
+
+    // routes.get('/comic/creator/role/:role', comicController.findCreatorsByRole)
+    it('Deve encontrar as comics por um pedaço do nome de um Criador', async () => {
+        const role = "colorist"
+        const response = await request(app).get(`/comic/creator/role/${role}`)
+
+        expect(response.status).toEqual(200)
+        expect(response.body[0].creators[0].name).toBe("Daniele Rudoni")
+        expect(response.body[0].creators[0].role).toBe(role)
+    })
 })
